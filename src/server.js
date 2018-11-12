84d2885172e83a848c15518e3830c3c2215bd474
Edwin <edwin_020193@hotmail.com>
2018-11-12 01:39:25 -0600

    TestSaveMethod

--- src/server.js
+++ src/server.js
@@ -2,7 +2,21 @@ const express = require('express');
 
 const user=require('./useCases/users');
 
+const User=require('./models/users').model;
+
 const app =express();
+
+let objUser={
+    firstNameOne:"extra",
+    firstNameTwo:"Gutierrez",
+    lastName:"Gutierrez",
+    address:"Porvenir 155",
+    email:"Misa@hotmail.com",
+    country:"Mexico",
+    city:"CDMX",
+    password:"test123",
+    phoneNumber:"32219188243"
+}
 
 app.get('/',(req,res)=>{
     res.json({
@@ -13,36 +27,59 @@ app.get('/',(req,res)=>{
 
 app.get('/users', async(req,res)=>{
     const users = await user.get();
-    console.log("Buenas tardes usuarios");
+    console.log("Consulta de usuarios");
     res.json({
         success:true,
-        message:"Hi users",
+        message:"Showing all users",
         payload:{
             users
         }
     });
 });
-let objUser={
-    firstNameOne:"PV",
-    firstNameTwo:"Gutierrez",
-    lastName:"Gutierrez",
-    address:"Porvenir 155",
-    email:"Misa@hotmail.com",
-    country:"Mexico",
-    city:"CDMX",
-    password:"test123",
-    phoneNumber:"32219188243"
-}
-app.post('/users', async(req,res)=>{
-    const newUsers =  await user.createUser(objUser);
-    console.log("Nuevo Usuario");
-    res.json({
-        success:true,
-        message:"User Inserted",
-        payload:{
-            newUsers
-        }
-    });
+
+app.get('/users/:id', async(req,res)=>{
+    console.log("Getting a user");
+    await user.findOne(req,res);
+});
+// app.post('/users', async(req,res)=>{
+//     const newUsers =  await user.createUser(objUser);
+//     console.log("Nuevo Usuario");
+//     res.json({
+//         success:true,
+//         message:"User Inserted",
+//         payload:{
+//             newUsers
+//         }
+//     });
+// });
+
+app.post('/users',async(req,res)=>{
+    const newUser = await new User();
+        newUser.firstNameOne=req.body.firstNameOne;
+        newUser.firstNameTwo=req.body.firstNameTwo;
+        newUser.lastName=req.body.lastName;
+        newUser.address=req.body.address;
+        newUser.email=req.body.email;
+        newUser.country=req.body.country;
+        newUser.city=req.body.city;
+        newUser.password=req.body.city;
+        newUser.phoneNumber=req.body.phoneNumber;
+        newUser.save((error,user)=>{
+            if(error){
+                console.error("There is an error : "+error);
+            }else{
+                console.log(user);
+                res.send({
+                    success:true,
+                    message:"New user from front",
+                    payload:{
+                        user
+                    }
+                });
+            }
+
+
+        });
 });
 
 module.exports = app;//Se exporta solo, dentro de un objeto da error
--- src/useCases/users/index.js
+++ src/useCases/users/index.js
@@ -10,12 +10,35 @@ async function get() {
     return AllUsers;
 }
 
+function findOne(req,res){
+    //console.log(req);
+    Users.findOne({
+        _id:req.params.id
+    }).exec((error,users)=>{
+            if(error){
+                console.log("There is an error : "+error);
+            }else{
+                console.log("User Found");
+                 console.log(users);
+                 res.json({
+                     users
+                 });
+                 return users;
+            }   
+    });
+    
+    
+}
+
 function createUser(objUser){
     const NewUsers= Users.create(objUser);
     return NewUsers;
 }
 
+
+
 module.exports = {
     get,
-    createUser
+    createUser,
+    findOne
 }
