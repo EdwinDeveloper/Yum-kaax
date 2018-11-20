44b91b7b77bdd3ebdb36828d5057754181d32f81
Edwin <edwin_020193@hotmail.com>
2018-11-20 00:49:38 -0600

    delete and put machine

--- back-end/src/useCases/machines/index.js
+++ back-end/src/useCases/machines/index.js
@@ -5,9 +5,11 @@ const getAllMachines = async () =>{
     return allMachines;
 }
 
-const createMachine = (dataMachine)=>{
-    const { serial_number } = dataMachine;
-    const existMachine = machinesModel.find({serial_number}).exec();
+const createMachine = async (dataMachine)=>{
+    const { id ,serial_number } = dataMachine;
+    console.log(serial_number);
+    const existMachine = await machinesModel.find({serial_number}).exec();
+    console.log(existMachine);
     const exist = existMachine.length > 0;
 
     if(exist) throw new Error('This machine already exist');
@@ -16,7 +18,14 @@ const createMachine = (dataMachine)=>{
     const machineCreated = newMachine.save();
     return machineCreated;
 }
+
+const deleteMachine = (id)=>{
+    const machineDeleted = machinesModel.findByIdAndDelete(id).exec();
+    return machineDeleted;
+}
+
 module.exports = {
     getAllMachines,
-    createMachine
+    createMachine,
+    deleteMachine
 }
